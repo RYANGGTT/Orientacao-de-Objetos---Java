aeee7766c953b6872870781bc185bae45646fdd9
RYANGGTT <fabricioryan.ma@gmail.com>
2026-01-29 20:28:05 -0300

    Começo a orientar classes e instanciar.

---
+++ src/Film.java
@@ -0,0 +1,9 @@
+public class Film {
+    String name;
+    int yearOfRealese;
+    boolean plan;
+    double assessment;
+    int totalAssessment;
+    int duration;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Film baseFilm = new Film();
+        baseFilm.name = "Um gênio indomável";
+        baseFilm.yearOfRealese = 1997;
+        baseFilm.duration = 110 ;
+        baseFilm.assessment = 8.7;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(baseFilm.assessment);
     }
 }
