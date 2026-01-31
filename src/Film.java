9ed04bbaa796f128cd337e2c889b032a2f1b9c53
RYANGGTT <fabricioryan.ma@gmail.com>
2026-01-30 21:13:21 -0300

    Começo a orientar classes e instanciar.

--- src/Film.java
+++ src/Film.java
@@ -2,8 +2,30 @@ public class Film {
     String name;
     int yearOfRealese;
     boolean plan;
-    double assessment;
-    int totalAssessment;
+    private double assessment;
+    private int totalAssessment;
     int duration;
 
+
+    void showTecnichalsheet(){ // metodo sem retorno
+        System.out.println("Nome do filme :" + name);
+        System.out.println("Ano de lançamento :" + yearOfRealese);
+        System.out.println("Nota de avaliação :" + assessment);
+        System.out.println("Duração em minutos :" + duration);
+    }
+
+    void userAssesment(double stars){ // metodo com argumento
+        assessment = assessment + stars ;
+        totalAssessment++;
+    }
+    double mediaAssessment (){ // retorna media das avaliaçoões
+        return assessment / totalAssessment ;
+
+    }
+
+    int getTotalAssessment(){
+        return totalAssessment;
+
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ public class Main {
         baseFilm.name = "Um gênio indomável";
         baseFilm.yearOfRealese = 1997;
         baseFilm.duration = 110 ;
-        baseFilm.assessment = 8.7;
 
-        System.out.println(baseFilm.assessment);
+        baseFilm.showTecnichalsheet();
+        baseFilm.userAssesment(10);
+        baseFilm.userAssesment(8);
+        baseFilm.userAssesment(7);
+
+        System.out.println(baseFilm.getTotalAssessment());
+        System.out.println (baseFilm.mediaAssessment());
+
     }
 }
